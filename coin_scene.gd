ea3deb6c52ad4a5ec4cf79b8344b40a0bf12c9af
Daniel Mau <daniel.b.mau@gmail.com>
2025-01-18 15:53:39 -0500

    Made it so that coin sprite changes when coin flip sound ends

--- coin_scene.gd
+++ coin_scene.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+var coin_sprite
 var coin_sprite_area
 var mouse_inside = false
  
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	coin_sprite = get_node("CoinSprite")
 	coin_sprite_area = get_node("CoinSprite/CoinSpriteArea")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,6 +14,12 @@ func _process(delta: float) -> void:
 	if mouse_inside == true && Input.is_action_just_pressed("mouse_click"):
 		Singleton.coin_flip_sound.play()
 		Singleton._flip_coin()
+	
+	if Singleton.result_finished == true:
+		if Singleton.coin_flip_result == "heads":
+				coin_sprite.texture = load("res://art/icon.svg")
+		else:
+			coin_sprite.texture = load("res://art/tails.png")
 
 func _on_coin_sprite_area_mouse_entered() -> void:
 	mouse_inside = true
--- singleton.gd
+++ singleton.gd
@@ -2,6 +2,7 @@ extends Node
 
 var coin_flip_sound
 var coin_flip_result
+var result_finished = false
 var rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
@@ -11,8 +12,14 @@ func _process(delta: float) -> void:
 	pass
 
 func _flip_coin() -> void:
+	result_finished = false
 	var random_number = rng.randi_range(0, 1)
 	if random_number == 1:
 		coin_flip_result = "heads"
 	else:
 		coin_flip_result = "tails"
+	print(coin_flip_result)
+
+func _on_coin_flip_sound_finished() -> void:
+	print("dnak")
+	result_finished = true
